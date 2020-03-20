a952ce832ae4a5460f7a4dc57c8f568eac1fcef8
Christian Rijke <hello@christianrijke.de>
2020-03-21 00:09:31 +0100

    Merge all collect all dimensions of a measure in case it appears in different statistics. Fixes #168

--- src/services/catalog/treeApiSchema.js
+++ src/services/catalog/treeApiSchema.js
@@ -10,7 +10,18 @@ export default async app => {
         Object.keys(statisticSchema.measures).forEach(measure => {
           measures[measure] = {
             ...data[statistic].measures[measure],
-            source: statisticSchema
+            source: {
+              title_de: statisticSchema.title_de,
+              valid_from: statisticSchema.valid_from,
+              periodicity: statisticSchema.periodicity,
+              name: statisticSchema.name,
+              url: statisticSchema.url
+            },
+            dimensions: {
+              // tree API workaround: collect and merge all dimensions of this measure
+              ...(measures[measure] ? measures[measure].dimensions : []),
+              ...data[statistic].measures[measure].dimensions
+            }
           }
           if (!mappings[measure]) {
             mappings[measure] = []
