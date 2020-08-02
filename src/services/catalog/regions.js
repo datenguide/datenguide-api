93b8081807c739ed92c19b568778eb21e688aabd
Christian Rijke <christian.rijke@ext.aperto.com>
2020-08-02 14:44:21 +0200

    Refactor region catalog service and implement children attribute to fetch an entire region tree.

--- src/services/catalog/regions.js
+++ src/services/catalog/regions.js
@@ -6,7 +6,7 @@ export const DEFAULT_PAGE_SIZE = 10
 
 const RAW_REGIONS = 'RAW_REGIONS'
 
-const getNuts = id => ({ 2: 1, 3: 2, 5: 3 }[id.length])
+const getNuts = (id) => ({ 2: 1, 3: 2, 5: 3 }[id.length])
 
 const isNuts = (id, nuts) =>
   nuts >= 1 &&
@@ -18,13 +18,8 @@ const isLau = (id, lau) => lau >= 1 && lau <= 2 && id.length >= 8 && id !== 'DG'
 
 const hasParent = (id, parentId) => id !== parentId && id.startsWith(parentId)
 
-const rawRegionsToObjects = (regions, ids = []) =>
-  Object.keys(regions)
-    .filter(id => ids.includes(id))
-    .map(id => ({ id, name: regions[id], nuts: getNuts(id) }))
-
-export default async app => {
-  const getRaw = async () => {
+export default async (app) => {
+  const loadRegions = async () => {
     const cached = app.cache.get(RAW_REGIONS)
     if (cached) {
       return cached
@@ -55,9 +50,44 @@ export default async app => {
     }
   }
 
+  const getMatchingRegionIds = async (parent, nuts, lau) => {
+    const regions = await loadRegions()
+    return Object.keys(regions)
+      .filter((id) => parent === undefined || hasParent(id, parent))
+      .filter((id) => nuts === undefined || isNuts(id, nuts))
+      .filter((id) => lau === undefined || isLau(id, lau))
+  }
+
+  const getRegionsByIds = async (ids = []) => {
+    const regions = await loadRegions()
+    return Object.keys(regions)
+      .filter((id) => ids.includes(id))
+      .map((id) => ({ id, name: regions[id], nuts: getNuts(id) }))
+  }
+
+  const fetchChildren = async (region) => {
+    let children
+    if (region.nuts === 3) {
+      return region
+    } else if (region.nuts === 2) {
+      children = await getMatchingRegionIds(region.id, 3)
+    } else if (region.nuts === 1) {
+      children = await getMatchingRegionIds(region.id, 2)
+      if (children.length === 0) {
+        // try to skip nuts 2
+        children = await getMatchingRegionIds(region.id, 3)
+      }
+    }
+    children = await getRegionsByIds(children)
+    children = await Promise.all(
+      children.map(async (r) => await fetchChildren(r))
+    )
+    return { ...region, children }
+  }
+
   const service = {
-    find: async params => {
-      Object.keys(params.query).forEach(key => {
+    find: async (params) => {
+      Object.keys(params.query).forEach((key) => {
         if (
           !['parent', 'nuts', 'lau', '$limit', '$skip', '$children'].includes(
             key
@@ -67,14 +97,8 @@ export default async app => {
         }
       })
 
-
-      const regions = await getRaw()
       const { parent, nuts, lau } = params.query
-      const ids = Object.keys(regions)
-        .filter(id => parent === undefined || hasParent(id, parent))
-        .filter(id => nuts === undefined || isNuts(id, nuts))
-        .filter(id => lau === undefined || isLau(id, lau))
-
+      const ids = await getMatchingRegionIds(parent, nuts, lau)
 
       const limit = Math.min(
         params.query.$limit || DEFAULT_PAGE_SIZE,
@@ -82,21 +106,31 @@ export default async app => {
       )
       const skip = params.query.$skip || 0
 
+      const regionPage = await getRegionsByIds(
+        ids.slice(skip * limit, skip * limit + limit)
+      )
+
+      const data =
+        params.query.$children === 'true'
+          ? await Promise.all(
+              regionPage.map(async (r) => await fetchChildren(r))
+            )
+          : regionPage
+
       return {
         total: ids.length,
         limit,
         skip,
-        data: rawRegionsToObjects(regions,  ids.slice(skip * limit, skip * limit + limit))
+        data,
       }
     },
-    get: async id => {
-      const regions = await getRaw()
-      const result = rawRegionsToObjects(regions, [id])
+    get: async (id) => {
+      const result = getRegionsByIds([id])
       return result.length === 1 ? result[0] : null
     },
-    setup: async feathersApp => {
-      await getRaw(feathersApp)
-    }
+    setup: async (feathersApp) => {
+      await loadRegions(feathersApp)
+    },
   }
 
   app.use('/catalog/regions', service)
@@ -105,20 +139,20 @@ export default async app => {
     .hooks({
       before: {
         all: [],
-        find: []
+        find: [],
       },
       after: {
         all: [],
-        find: []
+        find: [],
       },
       error: {
         all: [],
-        find: []
-      }
+        find: [],
+      },
     })
     .hooks({
       after: {
-        all: []
-      }
+        all: [],
+      },
     })
 }
